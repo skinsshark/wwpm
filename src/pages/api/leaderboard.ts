653dcf3807a0f98124bf110da77a50f241c3c0d2
Sharon Zheng <sharzheng@gmail.com>
2024-11-11 18:43:57 -0800

    update var names

--- src/pages/api/leaderboard.ts
+++ src/pages/api/leaderboard.ts
@@ -2,8 +2,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.SUPABASE_URL!;
+const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default async function handler(
